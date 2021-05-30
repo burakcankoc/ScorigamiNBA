0cb1848db46708c8957f9371ca7051794285baee
Burak Can Koç <47418924+burakcankoc@users.noreply.github.com>
2021-05-30 14:08:22 -0400

    Update Scorigami.R

--- Scorigami.R
+++ Scorigami.R
@@ -1,10 +1,7 @@
 library(nbastatR)
-library(dplyr)
-library(tidyr)
-library(ggplot2)
+library(tidyverse)
 
-logall = nbastatR::game_logs(seasons = 1947:2020, result_types = "team", season_types = c("Regular Season", "Playoffs"))
-
+logall = nbastatR::game_logs(seasons = 1947:2021, result_types = "team", season_types = c("Regular Season", "Playoffs"))
 
 logs = logall %>% 
   group_by(idGame) %>% 
@@ -19,14 +16,14 @@ logs %>%
   add_count() %>% 
   slice(n = n()) %>% 
   ungroup() %>% 
-  mutate(asd = cumsum(n)) %>% 
-  ggplot(aes(x = yearSeason, y = asd)) +
+  mutate(uniqueScore = cumsum(n)) %>% 
+  ggplot(aes(x = yearSeason, y = uniqueScore)) +
   geom_point(size = 2) +
   theme_bw() +
-  scale_x_continuous(breaks = seq(1950,2020,10), limits = c(1947,2020)) +
+  scale_x_continuous(breaks = seq(1950,2020,10), limits = c(1947,2021)) +
   #scale_y_continuous(breaks = s, limits = c(2,4))
   labs(title = "Cumulative Unique NBA Scores by Season",
-       subtitle = "Regular Season and Playoff Scores, 1947-2020",
+       subtitle = "Regular Season and Playoff Scores, 1947-2021",
        caption = "graph: @burakcankoc\nsource: basketball-reference.com",
        x = "Season",
        y = "Number of Unique NBA Scores") +
